edf00f40f2826bff26b05d70fa9e9d6d20a2a94c
cynthiaty <1575612658@qq.com>
2017-06-02 20:40:11 +0800

    init

---
+++ src/com/cynthiaty/Test.java
@@ -0,0 +1,10 @@
+package com.cynthiaty;  //打包命令
+
+public class Test {
+
+    public static void main(String[] args) {
+	    // write your code here
+        //System.out.println("Hello!");
+        System.out.println(args[0]);
+    }
+}
